a3d3da55a48ab6e30e9a25e541ac18052a657c77
Sarid10 <saridqureshi299@gmail.com>
2023-02-10 09:16:59 +0530

    login page added and added validations in book appointment, login

--- apps/login.py
+++ apps/login.py
@@ -3,3 +3,12 @@ import streamlit as st
 def app():
     st.title('Login')
 
+    email = st.text_input('Enter your email', placeholder='abc@gmail.com')
+
+    password = st.text_input('Enter your password', '', type='password')
+
+    if st.button('Login'):
+        if email == '' or password == '':
+            st.error("Please fill the credentials")
+        else:
+            st.success("login")
